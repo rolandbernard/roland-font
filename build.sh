ae147042136cf65033fa418dee77918dbaab1f16
Roland Bernard <rolli.bernard@gmail.com>
2020-08-28 15:24:52 +0200

    First working example

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-fontforge -script ./masters/generate.pe ./masters/Light-Regular.sfd ./masters/Light-Regular.ufo &> /dev/null
-fontforge -script ./masters/generate.pe ./masters/Bold-Regular.sfd ./masters/Bold-Regular.ufo &> /dev/null
+fontforge -script ./masters/generate.pe ./masters/Light.sfd ./masters/Light.ufo
+fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
 
 fontmake -m Roland.designspace -o variable --production-names --output-path Roland.ttf
