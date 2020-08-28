4285bb8155890a4c9739f0bd5278020514be0dfb
Roland Bernard <rolli.bernard@gmail.com>
2020-08-28 20:09:21 +0200

    Tested kurning

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
 fontforge -script ./masters/generate.pe ./masters/Light-Condensed.sfd ./masters/Light-Condensed.ufo
 fontforge -script ./masters/generate.pe ./masters/Light-Expanded.sfd ./masters/Light-Expanded.ufo
 fontforge -script ./masters/generate.pe ./masters/Bold-Expanded.sfd ./masters/Bold-Expanded.ufo
+fontforge -script ./masters/interpolate.pe ./masters/Light.sfd ./masters/Bold.sfd ./masters/Regular.ufo
 
 fontmake -m Roland.designspace -o variable --production-names --output-path Roland.ttf
