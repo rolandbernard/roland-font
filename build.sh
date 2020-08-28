bad9b7f614b7065df34677eca9bc1dc099de97e4
Roland Bernard <rolli.bernard@gmail.com>
2020-08-28 17:31:44 +0200

    Added the width axis

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 fontforge -script ./masters/generate.pe ./masters/Light.sfd ./masters/Light.ufo
 fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
+fontforge -script ./masters/generate.pe ./masters/Light-Condensed.sfd ./masters/Light-Condensed.ufo
 
 fontmake -m Roland.designspace -o variable --production-names --output-path Roland.ttf
