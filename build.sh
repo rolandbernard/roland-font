7b642fb8412e2e8a1d01c0159be36d28a18b52da
Roland Bernard <rolli.bernard@gmail.com>
2020-08-28 18:19:11 +0200

    Added extended

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 fontforge -script ./masters/generate.pe ./masters/Light.sfd ./masters/Light.ufo
 fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
 fontforge -script ./masters/generate.pe ./masters/Light-Condensed.sfd ./masters/Light-Condensed.ufo
+fontforge -script ./masters/generate.pe ./masters/Light-Expanded.sfd ./masters/Light-Expanded.ufo
+fontforge -script ./masters/generate.pe ./masters/Bold-Expanded.sfd ./masters/Bold-Expanded.ufo
 
 fontmake -m Roland.designspace -o variable --production-names --output-path Roland.ttf
