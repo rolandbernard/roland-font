2116e13f324fed766cc6e26902e00bc18cb4c855
Roland Bernard <rolli.bernard@gmail.com>
2020-08-31 23:07:16 +0200

    Fixed multiple bugs

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 
 mkdir -p ./build/masters_ufo/
 
-fontforge -script ./masters/generate.pe ./masters/Light.sfd ./build/masters_ufo/Light.ufo
-fontforge -script ./masters/generate.pe ./masters/Light-Condensed.sfd ./build/masters_ufo/Light-Condensed.ufo
-fontforge -script ./masters/generate.pe ./masters/Light-Expanded.sfd ./build/masters_ufo/Light-Expanded.ufo
+fontforge -script ./generate.pe ./masters/Light.sfd ./build/masters_ufo/Light.ufo
+fontforge -script ./generate.pe ./masters/Light-Condensed.sfd ./build/masters_ufo/Light-Condensed.ufo
+fontforge -script ./generate.pe ./masters/Light-Expanded.sfd ./build/masters_ufo/Light-Expanded.ufo
 
-fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./build/masters_ufo/Bold.ufo
-fontforge -script ./masters/generate.pe ./masters/Bold-Condensed.sfd ./build/masters_ufo/Bold-Condensed.ufo
-fontforge -script ./masters/generate.pe ./masters/Bold-Expanded.sfd ./build/masters_ufo/Bold-Expanded.ufo
+fontforge -script ./generate.pe ./masters/Bold.sfd ./build/masters_ufo/Bold.ufo
+fontforge -script ./generate.pe ./masters/Bold-Condensed.sfd ./build/masters_ufo/Bold-Condensed.ufo
+fontforge -script ./generate.pe ./masters/Bold-Expanded.sfd ./build/masters_ufo/Bold-Expanded.ufo
 
-# Generate the Regular instance in order to use it as the default
-fontmake -m regular.designspace -o ufo -i --production-names
 # Generate all instances as ttf
 fontmake -m roland.designspace -o ufo ttf -i --production-names --output-dir ./build/instances_ttf
 # Generate the variable font
