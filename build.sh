7aaa353fddaeb6c7434456d5c57878274741df51
Roland Bernard <rolli.bernard@gmail.com>
2020-08-31 21:54:06 +0200

    Added bold condensed (75%) to the build prosses

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 
 fontforge -script ./masters/generate.pe ./masters/Light.sfd ./masters/Light.ufo
-fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
 fontforge -script ./masters/generate.pe ./masters/Light-Condensed.sfd ./masters/Light-Condensed.ufo
 fontforge -script ./masters/generate.pe ./masters/Light-Expanded.sfd ./masters/Light-Expanded.ufo
+
+fontforge -script ./masters/generate.pe ./masters/Bold.sfd ./masters/Bold.ufo
+fontforge -script ./masters/generate.pe ./masters/Bold-Condensed.sfd ./masters/Bold-Condensed.ufo
 fontforge -script ./masters/generate.pe ./masters/Bold-Expanded.sfd ./masters/Bold-Expanded.ufo
 
 fontmake -m Roland.designspace -o variable --production-names --output-path Roland.ttf
