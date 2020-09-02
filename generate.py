0f9b5806d11e19d921dec110ddaa65f3dcd10725
Roland Bernard <rolli.bernard@gmail.com>
2020-09-02 20:23:37 +0200

    Added the monospace axis

--- generate.py
+++ generate.py
@@ -4,12 +4,16 @@ import os
 import fontTools.designspaceLib as designspace
 import fontforge
 
+def baseWidth(glyph):
+    return glyph.width - glyph.left_side_bearing - glyph.right_side_bearing
+
 os.makedirs("build/masters_ufo", exist_ok=True)
 
 family_name = "Roland"
 axises = [
     ["wgth", "weight", 0, 1000, 0],
     ["wdth", "width", 50, 150, 100],
+    ["mono", "monospace", 0, 1, 0],
     ["slnt", "slant", 0, 20, 0],
 ]
 designed_masters = [
@@ -18,7 +22,7 @@ designed_masters = [
     ["Light-Expanded", [ 0, 150 ]], 
     ["Bold", [ 1000, 100 ]], 
     ["Bold-Condensed", [ 1000, 75 ]], 
-    ["Bold-Expanded", [ 1000, 150 ]]
+    ["Bold-Expanded", [ 1000, 150 ]],
 ]
 all_masters = []
 instances = [
@@ -34,8 +38,14 @@ instances = [
     ],
     [
         ["", 0],
+    ],
+    [
+        ["", 0],
         ["Italic", 10],
-    ]
+    ],
+]
+special_instances = [
+    ["Mono", [400, 100, 1, 0]],
 ]
 
 # Generate UFOs
@@ -45,7 +55,8 @@ for master in designed_masters:
     font.fullname = family_name + " Variable"
     font.weight = "Variable"
     # Create auto width
-    font.selection.select("\"") # Select characters that I don't want to change
+    font["space"].width = int(400 * math.sqrt(master[1][1] / 100))
+    font.selection.select("\"", " ") # Select characters that I don't want to change
     font.selection.invert()
     font.autoWidth(100, minBearing=20)
     # Create auto kerning
@@ -62,6 +73,56 @@ for master in designed_masters:
     font.autoKern("Kerning-1", 100)
     font.generate("build/masters_ufo/" + master[0] + ".ufo")
     all_masters.append(master + [ font ])
+
+# Generate mono fonts
+for master in all_masters.copy():
+    font = fontforge.font()
+    # Find the parameters
+    target_width = 500 * master[1][1] / 100
+    origin_fonts = []
+    if master[1][0] == 0:
+        origin_fonts = [all_masters[0][2], all_masters[1][2], all_masters[2][2]]
+        target_width += 4
+    else:
+        origin_fonts = [all_masters[3][2], all_masters[4][2], all_masters[5][2]]
+        target_width += 40
+    for glyph in origin_fonts[0].glyphs():
+        interpolate_with = None
+        new_glyph = None
+        if baseWidth(glyph) > target_width:
+            interpolate_with = origin_fonts[1][glyph.glyphname]
+        else:
+            interpolate_with = origin_fonts[0][glyph.glyphname]
+        if abs(baseWidth(interpolate_with) - baseWidth(glyph)) > 50:
+            new_glyph = font.createInterpolatedGlyph(glyph, interpolate_with, (target_width - 40 - baseWidth(glyph)) / (baseWidth(interpolate_with) - baseWidth(glyph)))
+        else:
+            new_glyph = font.createInterpolatedGlyph(glyph, interpolate_with, 0)
+        new_glyph.left_side_bearing = 0
+        new_glyph.right_side_bearing = 0
+        if target_width > new_glyph.width:
+            side_bearing = int((target_width - new_glyph.width) / 2)
+            new_glyph.left_side_bearing = side_bearing
+            new_glyph.right_side_bearing = side_bearing
+        else:
+            new_glyph.transform([(target_width - 40) / new_glyph.width, 0, 0, 1, 0, 0])
+            new_glyph.left_side_bearing = 20
+            new_glyph.right_side_bearing = 20
+        new_glyph.width = int(target_width)
+    font.familyname = family_name
+    font.fullname = family_name + " Variable"
+    font.weight = "Variable"
+    font.ascent = 800
+    font.descent = 200
+    font.os2_xheight = 500
+    font.os2_capheight = 750
+    # We need a kerning lookup, to interpolate with
+    font.addLookup("Kerning", "gpos_pair", None, (("kern",(("DFLT",("dflt")), ("latn",("dflt")),)),))
+    font.addLookupSubtable("Kerning", "Kerning-1")
+    font["e"].addPosSub("Kerning-1", "X", 0)
+    font.generate("build/masters_ufo/Mono-" + master[0] + ".ufo")
+    all_masters.append(["Mono-" + master[0], master[1] + [ 1 ], font])
+    master[1].append(0)
+    
 
 # Generate slanted fonts
 for master in all_masters.copy():
@@ -92,11 +153,6 @@ for master in all_masters:
     s.location = dict()
     for i in range(0, len(axises)):
         s.location[axises[i][1]] = master[1][i]
-    if master == all_masters[0]:
-        s.copyLib = True
-        s.copyInfo = True
-        s.copyGroups = True
-        s.copyFeatures = True
     document.addSource(s)
 
 instances_resolved = []
@@ -110,6 +166,8 @@ for ax in instances:
     else:
         for value in ax:
             instances_resolved.append([value[0], [value[1]]])
+            
+instances_resolved += special_instances
 for instance in instances_resolved:
     instance[0] = instance[0].strip()
     if len(instance[0]) == 0:
